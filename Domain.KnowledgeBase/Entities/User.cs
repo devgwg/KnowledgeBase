57b97db4a138dc72553ebbfd0e98de6a0e916328
gwg <grzegorzgwardadev@gmail.com>
2024-02-07 23:52:09 +0100

    updated from 6.0 to 8.0 and added EntityFrameworkCore

---
+++ Domain.KnowledgeBase/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.KnowledgeBase.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string Email { get; private set; }
+    public string Username { get; private set; }
+    public string Password { get; private set; }
+    public string FullName { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+
+
+
+    public User(Guid id, string email, string username, string password, string fullName,
+    DateTime createdAt)
+    {
+        Id = id;
+        Email = email;
+        Username = username;
+        Password = password;
+        FullName = fullName;
+        CreatedAt = createdAt;
+    }
+
+}
