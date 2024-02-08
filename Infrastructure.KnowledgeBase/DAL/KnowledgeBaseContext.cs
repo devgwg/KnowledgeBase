d97f1fe96940e8a9388d878c8203b3376ab0f1a7
gwg <grzegorzgwardadev@gmail.com>
2024-02-09 00:03:27 +0100

    Add context and migration

---
+++ Infrastructure.KnowledgeBase/DAL/KnowledgeBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.KnowledgeBase.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.KnowledgeBase.DAL;
+
+public class KnowledgeBaseContext: DbContext
+{
+
+    public DbSet<User> Users { get; set; }
+
+    //Add Migration dotnet ef migrations add Init -o ./DAL/Migrations
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=WIN-UKHKSI6BDS2;Database=KnowledgeBaseDb;Trusted_Connection=True;Encrypt=false");
+    }
+}
+
