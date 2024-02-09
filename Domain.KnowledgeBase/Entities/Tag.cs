09207a0834557b4b89c8ffc907c877f97af6ecc3
gwg <grzegorzgwardadev@gmail.com>
2024-02-09 23:56:52 +0100

    Add Topic and Tag

---
+++ Domain.KnowledgeBase/Entities/Tag.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.KnowledgeBase.Entities;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; private set; }
+
+
+
+
+    public Tag(Guid id, string name, DateTime createdAt)
+    {
+        Id = id;
+        Name = name;
+        CreatedAt = createdAt;
+    }
+}
+
---
+++ Domain.KnowledgeBase/Entities/Topic.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.KnowledgeBase.Entities;
+
+public class Topic
+{
+
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Link { get; set; } // lista linkow
+    public string Status { get; set; } // Zrobic enum
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
+
